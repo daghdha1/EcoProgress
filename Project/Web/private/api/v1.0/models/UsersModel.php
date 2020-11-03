c2010f114d0fe82b8ed8c670eddf64cf1e0715ba
Marcelo Espinola <marcelo.rodriguez305@gmail.com>
2020-11-03 20:32:51 +0100

    Web (Backend) --> Añadidos modelos y entidades.

--- Project/Web/private/api/v1.0/models/UsersModel.php
+++ Project/Web/private/api/v1.0/models/UsersModel.php
@@ -1,2 +1,32 @@
 <?php
-class UsersModel extends BaseModel {}
+class UsersModel extends BaseModel {
+	
+	private $adapter;
+	// Constructor
+	public function __construct($table, $adapter) {
+        parent::__construct($table, $adapter);
+        $this->adapter = $adapter;
+    }
+	// getMeasures()
+	public function getUsers() {
+		// Query
+		$sql = "SELECT * FROM Users";
+		// Respuesta
+		$result = MyEntity::executeSql($sql);
+		// Devuelve el resultado, si no ha encontrado ninguna coincidencia, devuelve null
+		return $result;
+	}
+	
+	public function getUser($mail) {
+		// Escapamos los carácteres especiales
+		$strMail = mysqli_real_escape_string($this->adapter, $mail);
+		// Query
+		$sql = "SELECT * FROM Users WHERE mail = '$strMail' LIMIT 1";
+		// Respuesta
+		$result = MyEntity::executeSql($sql);
+		// Devuelve el resultado, si no ha encontrado ninguna coincidencia, devuelve null
+		return $result;
+	}
+	
+	
+}
