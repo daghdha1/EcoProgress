7551e044ac950578cc0752639b08cb45b49e32f2
Daghdha <daghdha1.dev@gmail.com>
2020-11-03 17:58:50 +0100

    Web (Backend) --> Singleton DB connection added

--- Project/Web/_schema/ecoprogress.sql
+++ Project/Web/_schema/ecoprogress.sql
@@ -1,4 +1,4 @@
--- create database ecoprogress;
+create database ecoprogress;
 
 use ecoprogress;
 
@@ -7,27 +7,24 @@ drop table if exists Sensors;
 drop table if exists Users;
 
 CREATE TABLE Users (
-mail varchar (40),
+mail varchar (40) PRIMARY KEY,
 name varchar (20),
 surname varchar (40),
-password varchar (20) not null,
-PRIMARY KEY (mail)
+password varchar (20) not null
 );
 
 CREATE TABLE Sensors (
-id varchar(10),
+id varchar(10) PRIMARY KEY,
 mail varchar (40),
 type varchar (15),
-PRIMARY KEY (id),
 foreign key (mail) references Users(mail)
 );
 
 CREATE TABLE Measures (
 value double,
-instant int,
-location varchar(27),
+instant int PRIMARY KEY,
+location varchar(30),
 idSensor varchar(10) not null,
-PRIMARY KEY (instant),
 foreign key (idSensor) references Sensors(id)
 );
 
@@ -37,10 +34,11 @@ insert into Users (mail, name, surname, password) values ('MEspinola@dot.gov', '
 insert into Users (mail, name, surname, password) values ('sotito@swimnow.com', 'Maria', 'Soto', 'ecoprogress');
 insert into Users (mail, name, surname, password) values ('daghdha@developer.com', 'Adrian', 'Soler Navarro', 'ecoprogress');
 
-insert into Sensors (id, mail, type) values ('1', 'sotito@swimnow.com', 'CO00001');
+insert into Sensors (id, mail, type) values ('1', 'daghdha@developer.com', 'CO00001'); -- Necesito este para hacer pruebas
+-- insert into Sensors (id, mail, type) values ('1', 'sotito@swimnow.com', 'CO00001');
 insert into Sensors (id, mail, type) values ('2', 'martagb@correo.com', 'CO00001');
 
 insert into Measures (value, instant, location, idSensor) values (1286, '1595559902', '38.995823, -0.177517', '1');
-insert into Measures (value, instant, location, idSensor) values (1206, '1573702829', '38.995823, -0.177517', '2');
+insert into Measures (value, instant, location, idSensor) values (1205, '1573702829', '38.995823, -0.177517', '2');
 
 select * from Measures;
