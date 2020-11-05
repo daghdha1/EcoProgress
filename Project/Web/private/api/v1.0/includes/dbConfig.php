5f50a1b9e0614fcec19709d322a99b0e243f39f9
Daghdha <daghdha1.dev@gmail.com>
2020-11-05 21:20:08 +0100

    Database --> dbConfig upload

---
+++ Project/Web/private/api/v1.0/includes/dbConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+	Database configuration
+    @author EcoProgress Team 04
+*/
+
+define ( 'HOST', 'localhost' );
+define ( 'USER', 'root' );
+define ( 'PASSWORD', 'ecoprogress2020' );
+define ( 'DATABASE', 'ecoprogress' );
+define ( 'PORT', 3306);
+
+define ( 'USER_CREATED_SUCCESSFULLY', 0 );
+define ( 'USER_CREATE_FAILED', 1 );
+define ( 'USER_ALREADY_EXISTED', 2 );
