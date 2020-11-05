99dba85f0d4cf43c820bca0ddc27e88d668f127e
Marcelo Espinola <marcelo.rodriguez305@gmail.com>
2020-11-05 18:07:47 +0100

    (Web) --> Airquality done

--- Project/Web/private/app/js/airQuality.js
+++ Project/Web/private/app/js/airQuality.js
@@ -1,2 +1,39 @@
 
+// En principio deberiamos de cambiar la cantidad de horas que solicitamos a la api
+function last24Hours(){
+        console.log(calculateQuality(getData(24)));
+}
 
+// 1 g/m3 = 1 mg/L = 1 ppm
+ function getData(x){
+
+    // suponiendo que son mg/m3 y que la bd los devuelve asi, si no habrá que parsear
+    let data =new Array();
+    data = [7,5,3,2,5,6,4,6,2];
+    return data;
+
+}
+
+function convertToMgM3(array){
+
+    let result = new Array();
+
+    array.forEach(element => {
+        result.push(element*1.146);
+    });
+
+    return result;
+}
+
+// media aritmetica clasica / promedio
+function calculateQuality(res){
+
+    let result = 0;
+    res.forEach(item => {
+        result +=item;
+    });
+
+    return (result/res.length);
+}
+
+last24Hours();
