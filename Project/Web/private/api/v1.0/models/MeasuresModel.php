a728c8e7ad57bb0f5d3a674ebdb3babed3176a5b
Marcelo Espinola <marcelo.rodriguez305@gmail.com>
2020-11-06 09:59:38 +0100

    (Web) --> Mobile Fixed post
    He cambiado el metodo de envío del retrofit, el objeto que devuelve retrofit ya no es una measure, es un objeto json. Hay que remodelar el pojo o no utilizarlo.

--- Project/Web/private/api/v1.0/models/MeasuresModel.php
+++ Project/Web/private/api/v1.0/models/MeasuresModel.php
@@ -23,8 +23,12 @@ class MeasuresModel extends BaseModel {
 	// postMeasure()
 	public function postMeasure($parameters) {
 		// Escapamos los carácteres especiales
-		//echo "------------> " . $parameters;
-		//print_R($parameters);
+		/*
+		echo "\n------------MeasuresModel-------------\n";
+		print_r($parameters);
+		echo "\n----------------------------------------\n";
+		*/
+		
 		$strValue = mysqli_real_escape_string($this->conn, $parameters['value']);
 		$strtimestamp = mysqli_real_escape_string($this->conn, $parameters['timestamp']);
 		$strLocation = mysqli_real_escape_string($this->conn, $parameters['location']);
