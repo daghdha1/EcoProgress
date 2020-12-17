e9b53068229419713dbcbb5d868f14162e9911fe
María <mariasp19.MS@gmail.com>
2020-12-17 15:47:13 +0100

    Server --> ActiveTimeUser

--- Project/Web/private/api/v1.0/models/UsersModel.php
+++ Project/Web/private/api/v1.0/models/UsersModel.php
@@ -32,4 +32,30 @@ class UsersModel extends BaseModel {
 		return $result;
 	}
 	
+	/* 
+    * Obtiene el tiempo total del usuario activo
+    *
+    * N, N -->
+    *                 			getActiveUser() <--
+    * <-- Active time:N, Nada
+    */
+	public function getActiveTimeUser($sensorID, $time) {
+		$strSensorID = mysqli_real_escape_string($this->conn, $sensorID);
+		$strTime = mysqli_real_escape_string($this->conn, $time);
+		// Query
+		/* Queremos que nos devuelva el resultado, es decir, la suma del tiempo activo que ha estado un usuario mandando datos */
+		$sql = "SELECT timestamp FROM Measures as m WHERE '$strSensorID' ORDER BY timestamp DESC;";
+		// Respuesta
+		$result = BaseEntity::executeSelectSql($sql);
+		$res = 0;
+		$resultado = 0;
+		for($i = 0; $i < count($result); $i++){
+			$res = $result[$i] - $result[$i+1];
+			if($res <= $strTime){
+				$resultado = $resultado + $res;
+			}
+		}
+		// Devuelve el resultado, si no ha encontrado ninguna coincidencia, devuelve null
+		return $resultado;
+	}
 }
