c279519a71d0f0e76c510e2e79f7125ad11ed212
Daghdha <daghdha1.dev@gmail.com>
2020-12-18 04:47:55 +0100

    WEB (Frontend) --> last minor fixes (STABLE)

--- Project/Web/private/app/js/ui_accountPanel.js
+++ Project/Web/private/app/js/ui_accountPanel.js
@@ -2,13 +2,14 @@ showUserData();
 
 function showUserData() {
     getUser((dataReceived) => {
+        console.log(dataReceived);
         fillInUserFields(dataReceived);
     }, "daghdha@developer.com");
 }
 
 function fillInUserFields(userData) {
-    document.getElementById("name").innerHTML = userData.name;
-    document.getElementById("surnames").innerHTML = userData.surnames;
-    document.getElementById("mail").innerHTML = userData.mail;
-    document.getElementById("devices").innerHTML = "C000001";
+    document.getElementById("l_name").innerHTML = userData.name;
+    document.getElementById("l_surnames").innerHTML = userData.surnames;
+    document.getElementById("l_mail").innerHTML = userData.mail;
+    document.getElementById("l_devices").innerHTML = "C000001";
 }
