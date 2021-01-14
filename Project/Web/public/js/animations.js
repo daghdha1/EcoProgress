9324f1f7df959decc7e4054d5abeb1f7d24cf5cc
Marcelo Espinola <marcelo.rodriguez305@gmail.com>
2021-01-14 23:09:00 +0100

    Added landing super guay y quitada basura

--- Project/Web/public/js/animations.js
+++ Project/Web/public/js/animations.js
@@ -1,16 +1,23 @@
 window.lottieAnimation = bodymovin.loadAnimation({
     container: animation, // ID del div
-    path: '/EcoProgress/Project/Web/public/media/animacion.json', // Ruta fichero .json de la animación
+    path: '/EcoProgress/Project/Web/public/media/animation.json', // Ruta fichero .json de la animación
     renderer: 'svg', // Requerido
     loop: true, // Opcional
     autoplay: false, // Opcional
-})
-
-function playAnimation(){
+    rendererSettings: {
+        filterSize: {
+            width: '50%',
+            height: '50%',
+            x: '-50%',
+            y: '-50%',
+        }
+    }
+}, )
+function playAnimation() {
     window.lottieAnimation.play();
 }
 
-function stopAnimation(){
+function stopAnimation() {
     window.lottieAnimation.stop();
     window.lottieAnimation.destroy();
 }
