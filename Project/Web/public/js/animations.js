67088086e0ed3ce3a437e3645e82a3d6086490e8
Marcelo Espinola <marcelo.rodriguez305@gmail.com>
2021-01-15 04:09:07 +0100

    Animaciones y tal

--- Project/Web/public/js/animations.js
+++ Project/Web/public/js/animations.js
@@ -1,23 +1,39 @@
-window.lottieAnimation = bodymovin.loadAnimation({
-    container: animation, // ID del div
-    path: '/EcoProgress/Project/Web/public/media/animation.json', // Ruta fichero .json de la animación
-    renderer: 'svg', // Requerido
-    loop: true, // Opcional
-    autoplay: false, // Opcional
-    rendererSettings: {
-        filterSize: {
-            width: '50%',
-            height: '50%',
-            x: '-50%',
-            y: '-50%',
-        }
-    }
-}, )
+let isAnimating = false;
+
 function playAnimation() {
+    if (!isAnimating)
+        loadAnimation();
     window.lottieAnimation.play();
+    window.lottieAnimationText.play();
+}
+
+function loadAnimation() {
+    isAnimating = true;
+
+    window.lottieAnimation = bodymovin.loadAnimation({
+        container: animation, // ID del div
+        path: '/EcoProgress/Project/Web/public/media/animation.json', // Ruta fichero .json de la animación
+        renderer: 'svg', // Requerido
+        loop: true, // Opcional
+        autoplay: false
+    });
+    window.lottieAnimationText = bodymovin.loadAnimation({
+        container: animationText, // ID del div
+        path: '/EcoProgress/Project/Web/public/media/animationText.json', // Ruta fichero .json de la animación
+        renderer: 'svg', // Requerido
+        loop: true, // Opcional
+        autoplay: false // Opcional
+
+    });
+
 }
 
 function stopAnimation() {
-    window.lottieAnimation.stop();
+
+    //window.lottieAnimation.stop();
     window.lottieAnimation.destroy();
+    //window.lottieAnimation.stop();
+    window.lottieAnimationText.destroy();
+
+    isAnimating = false;
 }
