035d709845a7e68199ede052174675eb580b8e75
Daghdha <daghdha1.dev@gmail.com>
2021-01-15 12:03:02 +0100

    WEB (Back-Front) --> AccountPanel updated (STABLE)

--- Project/Web/public/js/rest_login.js
+++ Project/Web/public/js/rest_login.js
@@ -16,31 +16,25 @@ function login() {
         // Enviamos la petición
         fetch(request).then(function(response) {
             if (response.ok) return response.json();
-            else return 'Ha habido un error en la conexión con el servidor';
+            else return null;
         }).then(function(json) {
-            switch (typeof json) {
-                case 'object':
-                    if (json[0].role === 'user') {
-                        // Guardamos datos básicos de usuario
-                        saveUserSession(json);
-                        // Redireccionamos a la página principal del usuario
-                        window.location.replace('./private/app/html/home.html');
-                    } else if (json[0].role === 'admin') {
-                        // Redireccionamos a la página de admin
-                        window.location.replace('./private/app/html/adminPanel.html');
-                    }
-                    hideModalPanel('loginPanel');
-                    break;
-                case 'string':
-                    alert(json);
-                    break;
-                default:
-            }
+            responseHandler(json, (data) => {
+                if (data[0].role === 'user') {
+                    // Guardamos datos básicos de usuario
+                    saveUserSession(data);
+                    // Redireccionamos a la página principal del usuario
+                    window.location.replace('./private/app/html/home.html');
+                } else if (data[0].role === 'admin') {
+                    // Redireccionamos a la página de admin
+                    window.location.replace('./private/app/html/adminPanel.html');
+                }
+                hideModalPanel('loginPanel');
+            });
         });
     }
 }
 
-function saveUserSession(userDataSession) {    
+function saveUserSession(userDataSession) {
     localStorage.setItem("mail", userDataSession[0].mail);
     localStorage.setItem("name", userDataSession[0].name);
 }
